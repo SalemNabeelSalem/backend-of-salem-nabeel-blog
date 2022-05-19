ecec71af01fec5ade77513e4d79cd835e70ba2fd
Salem Nabeel Salem <isalemnabeelsalem@gmail.com>
2022-05-19 23:09:47 +0300

    Create the Backend App using Expressjs

---
+++ backend/src/index.js
@@ -0,0 +1,15 @@
+/**
+ * import 'dotenv/config';
+ *
+ * import express from "express";
+ */
+require("dotenv").config();
+
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.listen(port, () => console.log(`server is running on port: ${port}`));
