3ed2486a845e8ee5413398484e8dd764dda1e684
Salem Nabeel Salem <isalemnabeelsalem@gmail.com>
2022-05-29 09:59:33 +0300

    Using Helmet for Securing Express App By Setting Various HTTP Headers

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,7 +6,15 @@ require("./configs/mongodb.config");
 
 const express = require("express");
 
+/**
+ * cros is a middleware that allows cross-origin requests.
+ * */
 const cros = require("cors");
+
+/**
+ * helmet is a middleware that helps secure your Express apps by setting various HTTP headers.
+ * */
+const helmet = require("helmet");
 
 /** require all routes */
 const TutorialRoutes = require("./routes/tutorial.routes");
@@ -17,15 +25,18 @@ const PORT = process.env.EXPRESS_SERVER_PORT || 5000;
 /**
  * cors -> cross-origin resource sharing
  *
- * enable all corse requests
- */
+ * enable all cors requests
+ * */
 app.use(cros());
 
 /** parse requests of content-type - application/json */
 app.use(express.json());
 
-/* parse requests of content-type - application/x-www-form-urlencoded */
+/** parse requests of content-type - application/x-www-form-urlencoded */
 app.use(express.urlencoded({ extended: true }));
+
+/** secure your express application by setting various HTTP headers */
+app.use(helmet());
 
 app.listen(PORT, () => console.log(`server is running on port: ${PORT}`));
 
