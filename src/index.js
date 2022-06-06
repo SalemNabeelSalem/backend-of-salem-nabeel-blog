af58a427e1c06d5b67bced358bb831a058ca131b
Salem Nabeel Salem <isalemnabeelsalem@gmail.com>
2022-06-06 23:58:19 +0300

    Adding Basic Http Authentication into Swagger UI using express-basic-auth Package

--- src/index.js
+++ src/index.js
@@ -7,19 +7,24 @@ require("./configs/mongodb.config");
 const express = require("express");
 
 /**
- * cros is a middleware that allows cross-origin requests.
+ * a middleware that allows cross-origin requests.
  * */
 const cros = require("cors");
 
 /**
- * helmet is a middleware that helps secure your Express apps by setting various HTTP headers.
+ * a middleware that helps secure your express apps by setting various http headers.
  * */
 const helmet = require("helmet");
 
 /**
- * morgan is a middleware for logging HTTP requests.
+ * a middleware for logging http requests.
  * */
 const morgan = require("morgan");
+
+/**
+ * a middleware that provides http basic authentication support for express.
+ * */
+const expressBasicAuth = require("express-basic-auth");
 
 /**
  * swagger documentation
@@ -27,7 +32,9 @@ const morgan = require("morgan");
 const swaggerUi = require("swagger-ui-express");
 const SwaggerSpecification = require("./configs/swagger.config");
 
-/** all routes */
+/**
+ * all routes of the application
+ * */
 const ProtectedRoutes = require("./routes/protected.routes");
 const UserRoutes = require("./routes/user.routes");
 const TutorialRoutes = require("./routes/tutorial.routes");
@@ -43,16 +50,24 @@ const PORT = process.env.EXPRESS_SERVER_PORT || 5000;
  * */
 app.use(cros());
 
-/** parse requests of content-type - application/json */
+/**
+ * parse requests of content-type - application/json
+ * */
 app.use(express.json());
 
-/** parse requests of content-type - application/x-www-form-urlencoded */
+/**
+ * parse requests of content-type - application/x-www-form-urlencoded
+ * */
 app.use(express.urlencoded({ extended: true }));
 
-/** secure your express application by setting various HTTP headers */
+/**
+ * secure your express application by setting various http headers.
+ * */
 app.use(helmet());
 
-/** log HTTP requests */
+/**
+ * log http requests on the console.
+ * */
 app.use(morgan("common"));
 
 app.listen(PORT, () => {
@@ -70,8 +85,27 @@ app.get("/", (req, res) => {
   res.json(response).status(200);
 });
 
-app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(SwaggerSpecification));
+const basicAuth = expressBasicAuth({
+  users: {
+    [process.env.BASIC_AUTH_USERNAME]: process.env.BASIC_AUTH_PASSWORD,
+  },
+  challenge: true,
+  unauthorizedResponse: (req) => ({
+    message: "you are not authorized to access this resource.",
+  }),
+});
+
+app.use(
+  "/api/docs",
+  basicAuth,
+  swaggerUi.serve,
+  swaggerUi.setup(SwaggerSpecification)
+);
+
 app.use("/api/v1", ProtectedRoutes);
+
 app.use("/api/v1/users", UserRoutes);
+
 app.use("/api/v1/tutorials", TutorialRoutes);
+
 app.use("/api/v1/authors", AuthorRoutes);
